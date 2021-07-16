01bab3a277245c6ede571ad4665986f5db4a496e
Lívia BRQ <liviadevolder@gmail.com>
2021-07-16 09:57:52 -0300

    Aula 1

---
+++ src/usuario/usuario.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { UsuarioService } from './usuario.service';
+
+@Controller('users')
+export class UsuarioController {
+
+    private usuarioService = new UsuarioService();
+
+    @Post()
+    public cria(@Body() usuario) {
+        const usuarioCriado = this.usuarioService.cria(usuario);
+
+        return usuarioCriado;
+    }
+}
